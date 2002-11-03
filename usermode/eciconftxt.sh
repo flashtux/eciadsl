5adc0821fef0e7fe19c000577966813ce1ac00aa
wwp <subscript@free.fr>
2002-11-03 17:06:11 +0000

    Added database files for modems and providers.

--- usermode/eciconftxt.sh
+++ usermode/eciconftxt.sh
@@ -100,22 +100,52 @@ case "$1" in
 "")
     menu1="==== What do you want to do|Make all configuration|Remove Dabusb module|Change .bin sync file"
 
-    providers="Select your provider|Wanadoo|Club Internet|9 Telecom|Cegetel|World Online|Telecom Italia|Tiscali Italia|Pipex UK|Bluewin|Belgacom|Bezeq Intl.|Infostrada|Sunrise|Econophone|Other|"
-    servers_dns1="*|193.252.19.3|194.117.200.15|212.30.96.108|194.6.128.3|212.83.128.3|212.216.112.112|195.130.224.18|158.43.240.4|195.186.1.111|195.238.2.21|192.115.106.11|193.70.192.25|194.158.230.53|212.53.96.200|0.0.0.0|"
-    servers_dns2="*|193.252.19.4|194.117.200.10|213.203.124.146|194.6.128.4|212.83.128.4|212.216.172.62|195.130.225.129|158.43.240.3|195.186.4.111|195.238.2.22|192.115.106.10|193.70.152.25|194.158.230.54|212.53.100.250|0.0.0.0|"
-
-    modems="Select your modem|ECI Hifocus/B-Focus|Eicon Diva|Ericsson hm120dp|Aztech 100U|Fujitsu FDX310|US Robotics 8500|Webpower Ipmdatacom|BT Voyager|Wisecom ad80usg or EA100|Xentrix USB|Zyxel Prestige 630-41|Topcom Webr@cer 850|Siemens Santis|GVC BB039|Digicom MichelAngelo USB|Askey USB-ADSL Modem|Archtek UGW-8000|Cypress Globespan ADSL USB G7000|Topcom XPlorer 850|Other"
-    vid1pid1="*|05472131|071dac81|08ea00c9|05090801|0e600101|0baf00e6|09150001|16900203|09150001|0e600100|05472131|09150001|09150001|09150001|05472131|16900205|09150001|09150001|05472131|00000000"
-    vid2pid2="*|09158000|0915ac82|091500ca|09150802|09150102|091500e7|09150002|09150204|09150002|09150101|09158000|09150002|09150002|09150002|09158000|09150206|09150002|09150002|09158000|00000000"
-
-    echo -e "\n***** Welcome to Eci Adsl Linux driver configuration *****\n"
-    echo -e "At any time, press Ctrl+C to quit this script without saving modifications.\n"
-    echo -e "Tip: you can run eciconftxt.sh with a .bin file as parameter to change\nyour .bin quickly.\n"
-
     if [ ! -d $CONF_DIR ]; then
         echo -e "Config directory not found !\n"
         exit 1
     fi
+
+	providers_db="$CONF_DIR/providers.db"
+    providers="Select your provider"
+    servers_dns1="*"
+    servers_dns2="*"
+	OLDIFS="$IFS"
+	IFS="
+"
+	for LINE in $(grep -v -E "[ \t]#" "$providers_db" | tr -s "\t" "|" | cut -d '|' -f 1); do
+		providers="$providers|$LINE"
+	done
+	for LINE in $(grep -v -E "[ \t]#" "$providers_db" | tr -s "\t" "|" | cut -d '|' -f 2); do
+		servers_dns1="$servers_dns1|$LINE"
+	done
+	for LINE in $(grep -v -E "[ \t]#" "$providers_db"| tr -s "\t" "|" | cut -d '|' -f 3); do
+		servers_dns2="$servers_dns2|$LINE"
+	done
+    providers="$providers|Other"
+    servers_dns1="$servers_dns1|0.0.0.0"
+    servers_dns2="$servers_dns2|0.0.0.0"
+
+	modems_db="$CONF_DIR/modems.db"
+    modems="Select your modem"
+    vid1pid1="*"
+    vid2pid2="*"
+	for LINE in $(grep -v -E "[ \t]#" "$modems_db" | tr -s "\t" "|" | cut -d '|' -f 1); do
+		modems="$modems|$LINE"
+	done
+	for LINE in $(grep -v -E "[ \t]#" "$modems_db" | tr -s "\t" "|" | cut -d '|' -f 2); do
+		vid1pid1="$vid1pid1|$LINE"
+	done
+	for LINE in $(grep -v -E "[ \t]#" "$modems_db" | tr -s "\t" "|" | cut -d '|' -f 3); do
+		vid2pid2="$vid2pid2|$LINE"
+	done
+	IFS="$OLDIFS"
+    modems="$modems|Other"
+    vid1pid1="$vid1pid1|00000000"
+    vid2pid2="$vid2pid2|00000000"
+
+    echo -e "\n***** Welcome to Eci Adsl Linux driver configuration *****\n"
+    echo -e "At any time, press Ctrl+C to quit this script without saving modifications.\n"
+    echo -e "Tip: you can run eciconftxt.sh with a .bin file as parameter to change\nyour .bin quickly.\n"
 
     $0 @menu@ "$menu1"
 	ret=$?
