ab32a159b30ced7000f42e6aae4297bd3649c52d
wwp <subscript@free.fr>
2002-11-06 13:06:43 +0000

    Now differentiate all BSD systems.

--- usermode/platform_guess.c
+++ usermode/platform_guess.c
@@ -5,38 +5,42 @@
 */
 
 #ifdef _WIN32 
-#ifndef __GNUC__
-/* MSVC/Borland */
-#define OS "Win32 non-GNU"
-#else
-/* Cygwin */
-#define OS "Win32+Cygwin"
-#endif
+ #ifndef __GNUC__
+  /* MSVC/Borland */
+  #define OS "Win32 non-GNU"
+ #else
+  /* Cygwin */
+  #define OS "Win32+Cygwin"
+ #endif
 #elif defined(__MACOS__)
-/* MacOS */
-#define OS "MacOS"
+ /* MacOS */
+ #define OS "MacOS"
 #elif defined(__MACOSX__)
-/* MacOS/X*/
-#define OS "MacOS/X"
+ /* MacOS/X*/
+ #define OS "MacOS/X"
 #elif defined(__BEOS__)
-/* BeOS */
-#define OS "BeOS"
+ /* BeOS */
+ #define OS "BeOS"
 #elif defined(__EMX__)
-/* OS/2 GCC */
-#define OS "OS/2"
+ /* OS/2 GCC */
+ #define OS "OS/2"
 #elif defined(__QNX__)
-/* QNX */
-#define OS "QNX"
+ /* QNX */
+ #define OS "QNX"
 #else
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
-#define OS "BSD"
-#else
-#ifdef __GNUC__
-#define OS "GNU/Linux"
-#else
-#define OS "undetermined"
-#endif
-#endif
+ #if defined(__FreeBSD__)
+  #define OS "FreeBSD"
+ #elif defined(__NetBSD__)
+  #define OS "NetBSD"
+ #elif defined(__OpenBSD__)
+  #define OS "OpenBSD"
+ #else
+  #ifdef __GNUC__
+   #define OS "GNU/Linux"
+  #else
+   #define OS "undetermined"
+  #endif
+ #endif
 #endif
 
 int main(int argc, char** argv)
