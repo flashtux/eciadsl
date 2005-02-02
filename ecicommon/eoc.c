e5024901e6a7c0c374a00576c74204698b0d0c76
Jean-Sébastien Valette <jeanseb.valette@free.fr>
2005-02-02 16:21:25 +0000

    mask was shifted by one and trailing 0 forgoteent (shift is BEFORE mask !)

--- ecicommon/eoc.c
+++ ecicommon/eoc.c
@@ -52,7 +52,7 @@ void eoc_init() {
 u_int16_t eoc_decode(unsigned char b1, unsigned char b2) {
 	printf("OEC.C - eco_decode - START [b1b2 : %02x%02x]\n", b1, b2);
 	printf("OEC.C - eco_decode - END     [b1b2 : %04x]\n", (((b1 >>2) & 0x3f ) | ((b2 << 5) & 0xfe)));
-	return (((b1 >>2) & 0x3f ) | ((b2 << 5) & 0xfe));
+	return (((b1 >>2) & 0x3f ) | ((b2 << 5) & 0x1fc0));
 }
 
 /*
